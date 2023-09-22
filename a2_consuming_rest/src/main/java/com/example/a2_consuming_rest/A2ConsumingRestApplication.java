d2f35e01f1696235fbea3bf2af4a54c77879dfb0
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2023-09-22 23:04:46 +0530

    added assignment 2

---
+++ a2_consuming_rest/src/main/java/com/example/a2_consuming_rest/A2ConsumingRestApplication.java
@@ -0,0 +1,35 @@
+package com.example.a2_consuming_rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class A2ConsumingRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(A2ConsumingRestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2ConsumingRestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "http://localhost:8080/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+
+}
